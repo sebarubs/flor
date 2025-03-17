0f4a63a0938f7e8e39bad9741e1b2c0684720227
sebarubs <huisasebastian24@gmail.com>
2025-03-17 16:29:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,61 +4,47 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particles = [];
-
-class Particle {
-    constructor(x, y, size, color, speedX, speedY) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
-        this.speedX = speedX;
-        this.speedY = speedY;
-    }
-
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.size *= 0.98; // Se reduce de tamaño poco a poco
-    }
-
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
+// Función para dibujar un pétalo
+function drawPetal(x, y, angle) {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(angle);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.quadraticCurveTo(20, -40, 40, 0);
+    ctx.quadraticCurveTo(20, 40, 0, 0);
+    ctx.fillStyle = "#ffcc00";
+    ctx.fill();
+    ctx.restore();
 }
 
-// Función para crear una animación de flor
-function createFlower(x, y) {
-    for (let i = 0; i < 20; i++) {
-        let speedX = (Math.random() - 0.5) * 2;
-        let speedY = (Math.random() - 0.5) * 2;
-        let size = Math.random() * 5 + 2;
-        let color = `hsl(${Math.random() * 50 + 40}, 100%, 50%)`;
-        particles.push(new Particle(x, y, size, color, speedX, speedY));
+// Función para dibujar un girasol
+function drawSunflower(x, y) {
+    // Pétalos girando
+    for (let i = 0; i < 12; i++) {
+        drawPetal(x, y, (Math.PI / 6) * i);
     }
+
+    // Centro del girasol
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI * 2);
+    ctx.fillStyle = "#663300";
+    ctx.fill();
+
+    // Tallo
+    ctx.beginPath();
+    ctx.moveTo(x, y + 20);
+    ctx.lineTo(x, y + 100);
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 6;
+    ctx.stroke();
 }
 
-// Animación de la flor
+// Animación del girasol
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    particles.forEach((particle, index) => {
-        particle.update();
-        particle.draw();
-        if (particle.size < 0.5) {
-            particles.splice(index, 1);
-        }
-    });
-
+    drawSunflower(canvas.width / 2, canvas.height / 2);
     requestAnimationFrame(animate);
 }
-
-// Evento para generar la flor al hacer clic
-canvas.addEventListener("click", (event) => {
-    createFlower(event.clientX, event.clientY);
-});
 
 animate();
